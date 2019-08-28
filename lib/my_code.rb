01d89ead58dccb398263c20a94eb7a2ddaa396eb
Eddie Parker <eddie.parker88@gmail.com>
2019-08-28 14:49:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,14 @@ def map(array)
   end
   new
 end
+
+
+def reduce_to_total(source_array, starting_point = 0)
+total = starting_point
+counter = 0 
+while counter < source_array.size do
+  yield(total += source_array[counter])
+  counter +=1 
+end
+total 
+end
